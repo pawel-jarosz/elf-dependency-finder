c3aab3ecfdf936a5b520aca4e918af1cd97d85c0
Paweł Jarosz <jarosz.pawel@outlook.com>
2025-06-02 22:29:45 +0200

    Add walkdir

--- src/elf_explorer/mod.rs
+++ src/elf_explorer/mod.rs
@@ -2,15 +2,32 @@
 
 mod elf_file;
 
-pub use elf_file::{ ElfFile, ElfType};
+use std::collections::HashMap;
+use walkdir::WalkDir;
+
+pub use elf_file::{ElfFile, ElfType};
 
 pub struct SoFileExplorer {
-
+    available_libraries: HashMap<String, (ElfFile, Vec<String>)>
 }
 
 impl SoFileExplorer {
     pub fn new() -> Self {
-        SoFileExplorer {}
+        SoFileExplorer {
+            available_libraries: HashMap::new()
+        }
+    }
+
+    pub fn add_from_location(&mut self, path: &str) -> usize {
+        let found_libraries = 0_usize;
+        for entry in WalkDir::new(path).follow_links(true) {
+            let entry = entry.unwrap();
+            if entry.path().file_name().unwrap().to_str().unwrap().ends_with(".so") {
+                
+            }
+
+        }
+        4
     }
 }
 
