7b203bca7871eb0cecb920938902c740bfc8a035
Paweł Jarosz <jarosz.pawel@outlook.com>
2025-06-02 21:58:59 +0200

    Add skeleton for the finding dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 
 mod configuration;
+mod elf_explorer;
+mod elf_explorer;
 
 // args[1] configuration file
 fn main() {
@@ -9,6 +11,10 @@ fn main() {
         eprint!("Missing configuration file name!\n");
         std::process::exit(1);
     }
-    configuration::Configuration::load_from_file(&args[1]);
+    let config = configuration::Configuration::load_from_file(&args[1]);
 
+    for file in &config.libraries {
+        let elf_file = elf_explorer::ElfFile::new(file);
+        let dependencies = elf_explorer::find_dependencies(&elf_file, &config.libraries_path);
+    }
 }
